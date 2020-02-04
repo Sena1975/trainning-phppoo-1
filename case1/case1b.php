5f750886382d2d538f0090fae7b42f6be1dd9389
diegosrtrainninggit <diegosr.trainning.git@gmail.com>
2020-02-04 17:23:08 -0300

    Sincronizando repositório

--- case1/case1b.php
+++ case1/case1b.php
@@ -4,19 +4,17 @@
 require 'produto.php';
 
 $p1 = new Produto();
-//$p1->nome = "aaaa"; // erro
-$p1->setNome("Iphone");
-$p1->setValor(50);
-
+#$p1->nome = "aaaa"; // erro
+$p1->setNome("aaaa");
 
 //var_dump($p1->nome); // erro
-//var_dump($p1->getNome());
+var_dump($p1->getNome());
 
 $f1 = new Fabricante();
 $f1->setNome("Apple");
 
 $p1->setFabricante($f1);
-print_r($p1);
+var_dump($p1);
 
-//echo "Fabricante: ", $p1->getFabricante()->getNome();
+echo "Fabricante: ", $p1->getFabricante()->getNome();
 ?>
